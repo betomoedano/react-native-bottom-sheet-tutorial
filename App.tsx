149e08db0aa925c7cc0214f810a3249e89d9118a
betomoedano <betomoedano@outlook.com>
2022-01-26 14:46:31 -0500

    example bottomSheetModal

--- App.tsx
+++ App.tsx
@@ -1,97 +1,151 @@
-import React, { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef, useMemo, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, Button} from 'react-native';
+import BottomSheet, { BottomSheetView, BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import ApplePay from './src/components/ApplePay';
 
-export default function App() {
+const App = () => {
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
-  const sheetRef = useRef<BottomSheet>(null);
-  const [isOpen, setIsOpen] = useState(true);
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
 
-  const snapPoints = ["40%",];
-
-  const handleSnapPress = useCallback((index) => {
-    sheetRef.current?.snapToIndex(index);
-    setIsOpen(true);
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
   }, []);
 
-
+  // renders
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: isOpen ? '#00000090' : '#fff'}]}>
-      <Text style={styles.subtitle}>{new Date().toString().slice(0, 11)}</Text>
-      <Text style={styles.title}>Today</Text>
-      <View style={styles.shadow}>
-         <Image style={[styles.image, {opacity: isOpen ? 0.2 : 1 }]} source={{ uri: 'https://i.guim.co.uk/img/media/8806177a01e00e4afc6d5f04ec4e99a0db9166fb/50_293_2969_1781/master/2969.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c99accd4e72bec8825ac783bb40043cf'}} />
+    <BottomSheetModalProvider>
+      <View style={styles.container}>
+        <Button
+          onPress={handlePresentModalPress}
+          title="Present Modal"
+          color="black"
+        />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
+        >
+          <View style={styles.contentContainer}>
+            <Text>Awesome 🎉</Text>
+          </View>
+        </BottomSheetModal>
       </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => handleSnapPress(0)}>
-        <Text style={{ color: '#0080FB', fontSize: 16, fontWeight: '600'}}>GET</Text>
-      </TouchableOpacity>
-      <BottomSheet
-        ref={sheetRef}
-        snapPoints={snapPoints}
-        enablePanDownToClose={true}
-        onClose={() => setIsOpen(false)}
-      >
-        <BottomSheetView>
-          <ApplePay />
-        </BottomSheetView>
-      </BottomSheet>
-    </SafeAreaView>
+    </BottomSheetModalProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    
-  },
-  title: {
-    fontSize: 32,
-    textAlign: "left",
-    margin: 10,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 18,
-    textAlign: "left",
-    marginLeft: 10,
-    color: "#aaa",
-    fontWeight: '500',
-    
-  },
-  image: {
-    width: "90%",
-    height: 400,
-    resizeMode: "cover",
-    alignSelf: "center",
-    borderRadius: 20,
-
-  },
-  shadow: {
-    marginTop: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: .55,
-    shadowRadius: 6.84,
-    elevation: 5,
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: '#f4f4f4',
-    width: 80,
-    height: 30,
-    alignItems: 'center',
+    padding: 24,
     justifyContent: 'center',
-    alignSelf: 'center',
-    borderRadius: 15,
-  }
+    backgroundColor: 'white',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
 });
+
+export default App;
+
+// export default function App() {
+
+//   const sheetRef = useRef<BottomSheet>(null);
+//   const [isOpen, setIsOpen] = useState(true);
+
+//   const snapPoints = ["40%",];
+
+//   const handleSnapPress = useCallback((index) => {
+//     sheetRef.current?.snapToIndex(index);
+//     setIsOpen(true);
+//   }, []);
+
+
+//   return (
+//     <SafeAreaView style={[styles.container, { backgroundColor: isOpen ? '#00000090' : '#fff'}]}>
+//       <Text style={styles.subtitle}>{new Date().toString().slice(0, 11)}</Text>
+//       <Text style={styles.title}>Today</Text>
+//       <View style={styles.shadow}>
+//          <Image style={[styles.image, {opacity: isOpen ? 0.2 : 1 }]} source={{ uri: 'https://i.guim.co.uk/img/media/8806177a01e00e4afc6d5f04ec4e99a0db9166fb/50_293_2969_1781/master/2969.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c99accd4e72bec8825ac783bb40043cf'}} />
+//       </View>
+//       <TouchableOpacity
+//         style={styles.button}
+//         onPress={() => handleSnapPress(0)}>
+//         <Text style={{ color: '#0080FB', fontSize: 16, fontWeight: '600'}}>GET</Text>
+//       </TouchableOpacity>
+//       <BottomSheet
+//         ref={sheetRef}
+//         snapPoints={snapPoints}
+//         enablePanDownToClose={true}
+//         onClose={() => setIsOpen(false)}
+//       >
+//         <BottomSheetView>
+//           <ApplePay />
+//         </BottomSheetView>
+//       </BottomSheet>
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "flex-start",
+    
+//   },
+//   title: {
+//     fontSize: 32,
+//     textAlign: "left",
+//     margin: 10,
+//     fontWeight: "bold",
+//   },
+//   subtitle: {
+//     fontSize: 18,
+//     textAlign: "left",
+//     marginLeft: 10,
+//     color: "#aaa",
+//     fontWeight: '500',
+    
+//   },
+//   image: {
+//     width: "90%",
+//     height: 400,
+//     resizeMode: "cover",
+//     alignSelf: "center",
+//     borderRadius: 20,
+
+//   },
+//   shadow: {
+//     marginTop: 20,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     shadowColor: "#000",
+//     shadowOffset: {
+//       width: 0,
+//       height: 5,
+//     },
+//     shadowOpacity: .55,
+//     shadowRadius: 6.84,
+//     elevation: 5,
+//   },
+//   button: {
+//     marginTop: 20,
+//     backgroundColor: '#f4f4f4',
+//     width: 80,
+//     height: 30,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     alignSelf: 'center',
+//     borderRadius: 15,
+//   }
+// });
